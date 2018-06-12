27a02756b47dcdd69191528904022885f6381e37
lipeng <lipeng@gmei.com>
2018-06-12 09:40:11 +0800

    strkeydict

---
+++ binarytree/node.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# create by oldman at 2018/6/8
+"""
+树的节点
+"""
+
+
+class Node(object):
+    """
+    树的节点类
+    """
+
+    def __init__(self, data):
+        self.data = data
+        self.lchild = None
+        self.rchild = None
