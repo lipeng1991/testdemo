8fc678d860349c070e21d5a67a519231d60271de
lipeng <lipeng@gmei.com>
2018-09-30 17:56:56 +0800

    最终版的sentence

--- 33_Comparison_of_iterable_objects_and_iterators.py
+++ 33_Comparison_of_iterable_objects_and_iterators.py
@@ -10,3 +10,26 @@
 __next__ 返回一个可用的元素，如果没有了元素，抛出StopIteration异常
 __iter__ 返回self，以便在应该使用可迭代对象的地方使用迭代器，例如在for循环中
 """
+import re
+import reprlib
+
+"""
+使用生成器函数实现Sentence
+"""
+
+RE_WORD = re.complie('\w+')
+
+
+class Sentence():
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findeall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for word in self.words:
+            yield word
+        return
+
