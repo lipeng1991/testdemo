c87ed0034b035918d5e339149a4f64c2b9f3962e
lipeng <lipeng@gmei.com>
2019-03-21 20:12:12 +0800

    括号匹配

--- 37_bracket_matching.py
+++ 37_bracket_matching.py
@@ -42,3 +42,19 @@ b = 'judkslajdklsa{}><'
 
 assert judgment_brackets_matching(a) == True
 assert judgment_brackets_matching(b) == False
+
+
+def isValid(s):
+    """
+    这种方式s字符串中不能出现非括号字符
+    :param s:
+    :return:
+    """
+    stack = []
+    paren_map = {'}': '{', ']': '[', ')': '(', '>': '<'}
+    for c in s:
+        if c not in paren_map:
+            stack.append(c)
+        elif not stack or stack.pop() != paren_map[c]:
+            return False
+    return not stack
---
+++ 58_backtracking_algorithm.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# create by oldman
+# Date: 2019/2/12
+
+"""
+回溯算法
+"""
+
+
