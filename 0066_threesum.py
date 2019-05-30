c7e4161333ca757cabd0aab0af57e35525e422d0
lipeng <lipeng@gmei.com>
2019-05-30 10:56:51 +0800

    最接近的三数之和/删除排序数组中重复的项

--- 0066_threesum.py
+++ 0066_threesum.py
@@ -16,6 +16,7 @@
 ]
 """
 
+
 def threeSum(nums):
     dict_nums = {}
     res = []
@@ -36,3 +37,58 @@ def threeSum(nums):
                 if i < thre < j:
                     res.append([i, j, thre])
     return res
+
+
+"""
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+
+与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+"""
+
+
+def threeSumClosest(nums, target):
+    nums.sort()
+    sum_value = float("inf")
+    n = len(nums)
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l = i + 1
+        r = n - 1
+        while l < r:
+            cur = nums[i] + nums[l] + nums[r]
+            if cur == target: return target
+            if abs(cur - target) < abs(sum_value - target):
+                sum_value = cur
+            if cur < target:
+                l += 1
+            if cur > target:
+                r -= 1
+    return sum_value
+
+
+"""
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+示例 1:
+
+给定数组 nums = [1,1,2],
+
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+"""
+
+
+def removeDuplicates(nums) -> int:
+    if len(nums) == 0: return 0
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1
