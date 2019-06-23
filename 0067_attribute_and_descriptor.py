5a242eddcae5d334235b32178b4cd3a7a345f9c1
lipeng <lipeng@gmei.com>
2019-06-23 20:02:14 +0800

    属性查找和描述符

--- 0067_attribute_and_descriptor.py
+++ 0067_attribute_and_descriptor.py
@@ -27,8 +27,7 @@ class Dog(Animal):
     # 会再次调用__getattribute__。这样就会陷入无限递归。
     # 可以使用super()方法避免这个问题。
     def __getattribute__(self, key):
-        print
-        "calling __getattribute__\n"
+        print("calling __getattribute__\n")
 
         return super(Dog, self).__getattribute__(key)
 
@@ -44,19 +43,19 @@ class Dog(Animal):
         print("calling __setattr__")
 
         super(Dog, self).__setattr__(name, value)
+    #
+    # def __delattr__(self, name):
+    #     print("calling __delattr__")
+    #
+    #     super(Dog, self).__delattr__(name)
 
-    def __delattr__(self, name):
-        print("calling __delattr__")
 
-        super(Dog, self).__delattr__(name)
-
-
-dog = Dog(1)
-print(dog.__dict__)
-
-print(Dog.__dict__)
-print(Animal.__dict__)
-
+# dog = Dog(1)
+# print(dog.__dict__)
+#
+# print(Dog.__dict__)
+# print(Animal.__dict__)
+#
 # print(dog.age)
 # print(dog.fly)
 # print(dog.run)
@@ -88,11 +87,43 @@ class MyClass(object):
     y = 5
 
 
-m = MyClass()
-print(m.__dict__)
-print(MyClass.__dict__)
+# m = MyClass()
+# print(m.__dict__)
+# print(MyClass.__dict__)
+# # print(m.x)
 # print(m.x)
-print(m.x)
-m.x = 30
-print(m.x)
-print(m.__dict__)
+# m.x = 30
+# print(m.x)
+# print(m.__dict__)
+
+
+# 使用描述符做延迟加载
+
+
+class lazy(object):
+    def __init__(self, func):
+        self.func = func  # lazy的实例属性func等于area方法对象
+
+    def __get__(self, instance, cls):
+        val = self.func(instance)  # 调用area方法计算出结果
+        setattr(instance, self.func.__name__, val)  # 将结果设置给c的area属性
+        return val
+
+
+class Circle(object):
+    def __init__(self, radius):
+        self.radius = radius
+
+    @lazy  # area = lazy(area) lazy描述符
+    def area(self):
+        print('evalute')
+        return 3.14 * self.radius ** 2
+
+
+c = Circle(4)
+print(c.radius)
+print(c.__dict__)
+print(c.area)
+print(c.__dict__)
+print(c.area)
+print(c.area)
