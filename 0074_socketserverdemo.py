fdc8604ec87170beb199ee78066b9cf42a2a1150
lipeng <lipeng@gmei.com>
2019-08-23 23:47:50 +0800

    半双工通信

--- 0074_socketserverdemo.py
+++ 0074_socketserverdemo.py
@@ -6,6 +6,14 @@ import socket
 import threading
 import time
 
+from pip._vendor.distlib.compat import raw_input
+
+"""
+1.单工版非常简单，只能客户端单方面向服务端发消息，服务端回复固定模板消息。
+2. 半双工实现是连接建立以后，服务器等待客户端发送消息，客户端发送消息后等待接收服务器，这样一来一回循环往复下去。直到出现quit，关闭连接。
+
+
+"""
 def server01():
     """
     socket编程单向server端
@@ -36,3 +44,51 @@ def server01():
         t.start()
 
 
+
+def sercer02():
+    """
+    半双工实现
+    :return:
+    """
+    host = '127.0.0.1'
+    port = 50001
+    busize = 1024
+    addr = (host, port)
+
+    def closetcnt(s):
+        s.close()
+        print("Session closing")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(addr)
+    s.listen(1)
+    try:
+        while True:
+            print("waiting for connecting....")
+            ts, caddr = s.accept()
+            print("...connection from :{}".format(caddr))
+            try:
+                while True:
+                    rdata = ts.recv(busize)
+                    if not rdata:
+                        continue
+                    elif rdata == 'quit':
+                        break
+                    else:
+                        print("from {}  {} \n {}".format(caddr[0], time.ctime(), rdata))
+                    while True:
+                        sdata = str(input('> '))
+                        if not sdata:
+                            break
+                        else:
+                            ts.send("from {}  {} \n {}".format(caddr[0], time.ctime(), sdata))
+                            break
+
+            except socket.error as  detail:
+                print(detail)
+            closetcnt(ts)
+
+    finally:
+        s.close
+
+sercer02()
--- 0075_socketclientdemo.py
+++ 0075_socketclientdemo.py
@@ -4,6 +4,7 @@
 # Date: 2019/8/23
 import socket
 
+
 def client1():
     """
     socket编程单向client端
@@ -19,3 +20,56 @@ def client1():
 
     s.send('exit')
     s.close()
+
+
+def  client2():
+    """
+    半双工实现
+    :return:
+    """
+    host = '127.0.0.1'
+    port = 50001
+    busize = 1024
+    addr = (host, port)
+    tryCon = 0
+
+    tcps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            tcps.connect(addr)
+        except:
+            print(u'正在尝试连接远程主机')
+
+            tryCon +=1
+            if tryCon ==3:
+                print(u"无法连接上远程主机，请稍后再试")
+                exit()
+        else:
+            break
+    print(u"登录成功(通讯结束请输入'quiet'退出)\n")
+    try:
+        while True:
+            data = str(input('> '))
+            if not data:
+                continue
+            elif data=='quit':
+                tcps.send(data)
+                break
+            else:
+                tcps.send(data)
+
+
+            while True:
+                data = tcps.recv(busize)
+                if not data:
+                    continue
+                else:
+                    print(data)
+                    break
+
+    except socket.error as e:
+        print("session closing")
+        print(e)
+    tcps.close()
+
+client2()
