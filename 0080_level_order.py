37cc192595b390d23d0877c4a20f77254e35c299
lipeng <lipeng@gmei.com>
2019-08-28 20:34:09 +0800

    二叉树的层次遍历

---
+++ 0080_level_order.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# create by oldman
+# Date: 2019/8/28
+"""
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+"""
+import collections
+
+
+def  levelOrder(root):
+    if not root :
+        return []
+    result = []
+    queue = collections.deque()
+    queue.append(root)
+
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for _ in range(level_size):
+            node = queue.popleft()
+            current_level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(current_level)
+    return result
