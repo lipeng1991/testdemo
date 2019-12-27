92f0472c006d8d1501c8a75285b9be493d120fc1
li peng <li.peng_3335@immomo.com>
2019-12-28 00:32:07 +0800

    观察者模式开发

--- 0093_observer_pattern.py
+++ 0093_observer_pattern.py
@@ -5,6 +5,7 @@
 @time: 2019/12/23
 """
 from abc import ABCMeta, abstractmethod
+import unittest
 
 """
 观察者模式
@@ -41,17 +42,53 @@ class DisplayEment(metaclass=ABCMeta):
 
 class WeatherData(Subject):
     def __init__(self):
-        self.observers = set()
+        self.observers: set[Observer] = set()
         self.temp = 0
         self.humidity = 0
         self.pressure = 0
 
     def register_observer(self, o: Observer):
-        self.observers.append(o)
+        self.observers.add(o)
 
     def remove_observer(self, o: Observer):
         if o in self.observers:
             self.observers.remove(o)
 
     def notify_observers(self):
-        pass
+        for observe in self.observers:
+            observe.update(self.temp, self.humidity, self.pressure)
+
+    def measurements_changed(self):
+        self.notify_observers()
+
+    def set_weather_data(self, temp, humidity, pressure):
+        self.temp = temp
+        self.humidity = humidity
+        self.pressure = pressure
+        self.measurements_changed()
+
+
+class CurrentConditionsDisplay(Observer, DisplayEment):
+    pressure: object
+    humidity: object
+    temp: object
+
+    def __init__(self, weather_data: WeatherData):
+        self.weather_data = weather_data
+        weather_data.register_observer(self)
+
+    def update(self, temp, humidity, pressure):
+        self.temp = temp
+        self.humidity = humidity
+        self.pressure = pressure
+        self.display()
+
+    def display(self):
+        print(f"Current conditions:{self.temp}, degrees and {self.humidity},")
+
+
+class TestWeatherStation(unittest.TestCase):
+    def test_weather_station(self):
+        weather_data = WeatherData()
+        CurrentConditionsDisplay(weather_data)
+        weather_data.set_weather_data(2, 3, 5)
---
+++ 0094_decorator_pattern.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+""" 
+@author: lipeng
+@file: 0094_decorator_pattern.py 
+@time: 2019/12/27
+"""
